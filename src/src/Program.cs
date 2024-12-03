4bda852b8ce9541b00b43c8191cf49f789bb583c
BekzodAkhmadov <ahmadovbekzod94@gmail.com>
2024-12-03 18:48:19 +0500

    Code Added

--- src/src/Program.cs
+++ src/src/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+
+            var ones = number % 10;
+            var tens = number / 10 % 10;
+            var hundereds = number / 100 % 10;
+            var thousands = number / 1000 % 10;
+
+            var result = ones + tens + hundereds + thousands;
+
+            Console.WriteLine(result);
         }
     }
 }
